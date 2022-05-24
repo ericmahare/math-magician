b75a1cd0a9accf9e330e4155dd8a84073b7ac25e
ericmahare <erikmahare@gmail.com>
2022-05-24 17:56:15 +0300

    refactor UI interface

--- src/components/CalcUi.js
+++ src/components/CalcUi.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class CalcUi extends Component {
   constructor(props) {
@@ -8,33 +9,64 @@ class CalcUi extends Component {
   }
 
   render() {
+    const {
+      operate, total, operation, next,
+    } = this.props;
     return (
       <div className="calc-container">
-        <span className="result">0</span>
-        <span className="content-span position">
+        <p className="result">
+          <span>{total}</span>
+          <span>{operation}</span>
+          <span>{next}</span>
+        </p>
+        <button type="button" className="content-span position" name="AC" onClick={(e) => operate(e.target.name)}>
           AC
-        </span>
-        <span className="content-span position">+/-</span>
-        <span className="content-span position">%</span>
-        <span className="position aside">/</span>
-        <span className="content-span position">7</span>
-        <span className="content-span position">8</span>
-        <span className="content-span position">9</span>
-        <span className="position aside">x</span>
-        <span className="content-span position">4</span>
-        <span className="content-span position">5</span>
-        <span className="content-span position">6</span>
-        <span className="position aside">-</span>
-        <span className="content-span position">1</span>
-        <span className="content-span position">2</span>
-        <span className="content-span position">3</span>
-        <span className="position aside">+</span>
-        <span className="zero position">0</span>
-        <span className="content-span position">.</span>
-        <span className="position aside">=</span>
+        </button>
+        <button type="button" className="content-span position" name="+/-" onClick={(e) => operate(e.target.name)}>+/-</button>
+        <button type="button" className="content-span position" name="%" onClick={(e) => operate(e.target.name)}>%</button>
+        <button type="button" name="÷" className="position aside" onClick={(e) => operate(e.target.name)}>÷</button>
+        <button
+          className="content-span position"
+          type="button"
+          name="7"
+          onClick={(e) => operate(e.target.name)}
+        >
+          7
+        </button>
+        <button type="button" name="8" className="content-span position" onClick={(e) => operate(e.target.name)}>8</button>
+        <button type="button" className="content-span position" name="9" onClick={(e) => operate(e.target.name)}>9</button>
+        <button name="x" type="button" className="position aside" onClick={(e) => operate(e.target.name)}>x</button>
+        <button name="4" type="button" className="content-span position" onClick={(e) => operate(e.target.name)}>4</button>
+        <button name="5" type="button" className="content-span position" onClick={(e) => operate(e.target.name)}>5</button>
+        <button name="6" type="button" className="content-span position" onClick={(e) => operate(e.target.name)}>6</button>
+        <button name="-" type="button" className="position aside" onClick={(e) => operate(e.target.name)}>-</button>
+        <button type="button" name="1" className="content-span position" onClick={(e) => operate(e.target.name)}>1</button>
+        <button type="button" name="2" className="content-span position" onClick={(e) => operate(e.target.name)}>2</button>
+        <button type="button" name="3" className="content-span position" onClick={(e) => operate(e.target.name)}>3</button>
+        <button type="button" name="+" className="position aside" onClick={(e) => operate(e.target.name)}>+</button>
+        <button type="button" name="0" className="zero position" onClick={(e) => operate(e.target.name)}>0</button>
+        <button type="button" name="." className="content-span position" onClick={(e) => operate(e.target.name)}>.</button>
+        <button type="button" name="=" className="position aside" onClick={(e) => operate(e.target.name)}>=</button>
       </div>
     );
   }
 }
+
+CalcUi.propTypes = {
+  operate: PropTypes.func,
+  total: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  operation: PropTypes.string,
+  next: PropTypes.string,
+};
+
+CalcUi.defaultProps = {
+  operate: 'unknown',
+  total: 0,
+  operation: 0,
+  next: 0,
+};
 
 export default CalcUi;
