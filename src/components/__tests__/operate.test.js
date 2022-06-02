23c65a1e865df98a246f9a158e40a88e534ccf0a
ericmahare <erikmahare@gmail.com>
2022-06-02 18:18:27 +0300

    add tests

---
+++ src/components/__tests__/operate.test.js
@@ -0,0 +1,38 @@
+import operate from '../../logic/operate';
+
+describe('check operations entered', () => {
+  test('multiplication(X)', () => {
+    const result = operate('2', '2', 'x');
+    expect(result).toEqual('4');
+  });
+
+  test('Substracton(-)', () => {
+    const result = operate('8', '4', '-');
+    expect(result).toEqual('4');
+  });
+
+  test('Addition(+)', () => {
+    const result = operate('9', '9', '+');
+    expect(result).toEqual('18');
+  });
+
+  test('Division(÷)', () => {
+    const result = operate('50', '5', '÷');
+    expect(result).toEqual('10');
+  });
+
+  test('Division(÷)', () => {
+    const result = operate('11', '0', '÷');
+    expect(result).toEqual("Can't divide by 0.");
+  });
+
+  test('Modulus(%)', () => {
+    const result = operate('22', '11', '%');
+    expect(result).toEqual('0');
+  });
+
+  test('Modulus(%)', () => {
+    const result = operate('22', '0', '%');
+    expect(result).toEqual("Can't find modulo as can't divide by 0.");
+  });
+});
