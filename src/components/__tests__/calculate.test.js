4c7d2e09ef59a2244240c7466aef884d9b8a4cc7
ericmahare <erikmahare@gmail.com>
2022-06-02 23:36:48 +0300

    fix typo errors

--- src/components/__tests__/calculate.test.js
+++ src/components/__tests__/calculate.test.js
@@ -1,4 +1,4 @@
-import calculate from '../../logic/calculate';
+import calculate from '../../logic/Calculate';
 
 describe('AC pressed', () => {
   test('Clear everything', () => {
--- src/components/__tests__/operate.test.js
+++ src/components/__tests__/operate.test.js
@@ -1,4 +1,4 @@
-import operate from '../../logic/operate';
+import operate from '../../logic/Operate';
 
 describe('check operations entered', () => {
   test('multiplication(X)', () => {
